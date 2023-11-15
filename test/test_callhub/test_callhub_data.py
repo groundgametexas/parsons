65d141c11971bcb3a4f9e316605c04e84c60a86d
mattkrausse <106627640+mattkrausse@users.noreply.github.com>
2023-11-15 09:19:36 -0800

    added some sample json response

---
+++ test/test_callhub/test_callhub_data.py
@@ -0,0 +1,36 @@
+sample_users_response = {
+    "count": 1,
+    "next": None,
+    "previous": None,
+    "results": [
+        {
+            "url": "https://api.callhub.io/v1/users/6783/",
+            "username": "readme",
+            "email": "hello@callhub.io",
+            "bphones": ["https://api.callhub.io/v1/bphone/49765/"],
+            "phonebooks": [
+                "https://api.callhub.io/v1/phonebooks/78220/",
+                "https://api.callhub.io/v1/phonebooks/72819/",
+            ],
+            "voice_broadcasts": ["https://api.callhub.io/v1/voice_broadcasts/22604/"],
+            "sms_campaigns": [
+                "https://api.callhub.io/v1/sms_campaigns/51262/",
+                "https://api.callhub.io/v1/sms_campaigns/17466/",
+                "https://api.callhub.io/v1/sms_campaigns/12873/",
+                "https://api.callhub.io/v1/sms_campaigns/113459/",
+                "https://api.callhub.io/v1/sms_campaigns/10564/",
+                "https://api.callhub.io/v1/sms_campaigns/184564/",
+            ],
+            "callcenter_campaigns": [
+                "https://api.callhub.io/v1/callcenter_campaigns/2441/",
+                "https://api.callhub.io/v1/callcenter_campaigns/1024/",
+                "https://api.callhub.io/v1/callcenter_campaigns/9450/",
+                "https://api.callhub.io/v1/callcenter_campaigns/14562/",
+                "https://api.callhub.io/v1/callcenter_campaigns/1700/",
+                "https://api.callhub.io/v1/callcenter_campaigns/6324/",
+                "https://api.callhub.io/v1/callcenter_campaigns/6124/",
+                "https://api.callhub.io/v1/callcenter_campaigns/8149/",
+            ],
+        }
+    ],
+}
