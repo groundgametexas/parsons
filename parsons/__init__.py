f4b4b61e4f9592ebadb0b23b90f77ccc56738c77
mattkrausse <106627640+mattkrausse@users.noreply.github.com>
2023-11-15 08:52:31 -0800

    adding skeleton for callhub connector

--- parsons/__init__.py
+++ parsons/__init__.py
@@ -42,6 +42,7 @@ for module_path, connector_name in (
     ("parsons.bluelink", "Bluelink"),
     ("parsons.box.box", "Box"),
     ("parsons.braintree.braintree", "Braintree"),
+    ("parsons.callhub.callhub", "CallHub"),
     ("parsons.capitol_canary.capitol_canary", "CapitolCanary"),
     ("parsons.civis.civisclient", "CivisClient"),
     ("parsons.controlshift.controlshift", "Controlshift"),
