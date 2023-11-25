856659e2294ea2c60a5558b2f332d299f9e5ca24
mattkrausse <106627640+mattkrausse@users.noreply.github.com>
2023-11-24 17:55:18 -0800

    updated test and added todo

--- test/test_callhub/test_callhub.py
+++ test/test_callhub/test_callhub.py
@@ -3,15 +3,18 @@ import unittest
 import requests_mock
 from test_callhub_data import fake_contacts_data
 
+# TODO: This test seems to be hitting a sandbox API. We should mock the API response instead.
+
 
 class TestCallHub(unittest.TestCase):
     def setUp(self):
-        self.ch = CallHub("mykey")
+        self.ch = CallHub(api_key="test1234")
 
     @requests_mock.Mocker()
     def test_get_contacts(self, m):
         # Test that contacts are returned correctly.
-        m.get(requests_mock.ANY, json=fake_contacts_data)
+        m.get("https://api.callhub.io/v1/contacts/", json=fake_contacts_data)
+
         tbl = self.ch.get_contacts()
 
         self.assertEqual(tbl.num_rows, 10)
